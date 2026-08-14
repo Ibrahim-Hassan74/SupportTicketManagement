40061fb9d53180729fc1dda8a30180e05e4b52d5
Ibrahim-Hassan74 <ih3046376@gmail.com>
2026-08-14 06:31:31 +0300

    feat: add global exception handling and clean up core project

---
+++ SupportTicketManagement.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using SupportTicketManagement.Core.Helper;
+using System.Net;
+using System.Text.Json;
+
+namespace SupportTicketManagement.API.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IHostEnvironment _host;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, IHostEnvironment host)
+        {
+            _next = next;
+            _host = host;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                ApplySecurity(httpContext);
+
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                httpContext.Response.ContentType = "application/json";
+                var response = _host.IsDevelopment() ?
+                    ApiResponseFactory.InternalServerError(ex.Message, new List<string> { ex.StackTrace }) :
+                    ApiResponseFactory.InternalServerError("Internal Server Error");
+
+                var json = JsonSerializer.Serialize(response);
+                await httpContext.Response.WriteAsync(json);
+            }
+        }
+
+        private void ApplySecurity(HttpContext context)
+        {
+            context.Response.Headers["X-Content-Type-Options"] = "nosniff";
+            context.Response.Headers["X-XSS-Protection"] = "1;mode=block";
+            context.Response.Headers["X-Frame-Options"] = "DENY";
+        }
+
+    }
+    /// <summary>
+    /// Exception Handling Middleware Extensions
+    /// </summary>
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionHandlingMiddlewareExtensions
+    {
+        /// <summary>
+        /// Use Exception Handling Middleware
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlingMiddleware>();
+        }
+    }
+}
